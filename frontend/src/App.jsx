54401ea6da668c03ddd9a8ab3c357034d61f3249
JosVuHuynh <vuhuynh9x@gmail.com>
2022-07-18 14:12:02 +0700

    add FE login register watch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Home from './pages/home/Home'
 import "./app.scss"
+import Watch from './pages/watch/Watch'
+import Register from './pages/register/Register'
+import Login from './pages/login/Login'
 const App = () => {
   return (
-    <Home/>
+    // <Home/>
+    //<Watch/>
+    //<Register/>
+    <Login/>
   )
 }
 
